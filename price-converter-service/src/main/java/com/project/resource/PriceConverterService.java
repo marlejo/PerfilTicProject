5fbdacaf4d5524a6c442a5e3d633d232e18c6023
marlejo <marlejo1984@gmail.com>
2019-09-29 16:15:59 -0500

    Final commit

--- price-converter-service/src/main/java/com/project/resource/PriceConverterService.java
+++ price-converter-service/src/main/java/com/project/resource/PriceConverterService.java
@@ -1,6 +1,7 @@
 package com.project.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,14 +13,20 @@ public class PriceConverterService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@RequestMapping("/")
-	public String getPrice() {
+	@RequestMapping("/{amountCOP}")
+	public Double getPrice(@PathVariable("amountCOP")Double amountCOP) {
 		String s = restTemplate.getForObject("http://apilayer.net/api/live?"
 				+ "access_key=955b7265fd9c05905231169f9c45669c&"
 				+ "currencies=COP&"
 				+ "source=USD&"
 				+ "format=1", String.class);
-		return s;
+		String[] parts=s.split("USDCOP");
+		parts[1]=parts[1].replace("}", "");
+		parts[1]=parts[1].replace("\"", "");
+		parts[1]=parts[1].replace(":", "");
+		//System.out.println(parts[1]);
+		
+		return Double.parseDouble(parts[1])*amountCOP;
 	}
 
 }

ae7ca7844961540688af27536acc42673342ac86
marlejo <marlejo1984@gmail.com>
2019-09-29 09:02:53 -0500

    MicroServices creation

---
+++ add-product-service/src/main/java/com/project/AddProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddProductServiceApplication.class, args);
+	}
+
+}
---
+++ photo-storage-service/src/main/java/com/project/PhotoStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoStorageServiceApplication.class, args);
+	}
+
+}
